d75265df2fc6473c84319ef9b2f8247bfe01f349
leonardo nunes <leonardo.n.nogueira@aluno.senai.br>
2024-11-09 11:06:03 -0300

    mensagem

---
+++ javascript/atividade/script.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector(".titulo")
+titulo.textContent = "Bruno Daniel Gomes Correia"
+titulo.getElementsByClassName.textAlign = "Center" 
+
+const paragrafo = document.querySelector(".paragrafo")
+paragrafo.textContent = "Bruno Daniel Gomes"
+paragrafo.ElementsByid.textAlign = "Center"
